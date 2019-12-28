2908fc85636d932b1275523de88d0b129e8d070a
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-28 10:36:27 -0500

    Test constructors (fails to compile)

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace motorway_classes
     {
         static void Main(string[] args)
         {
+            // test constructors
+            Motorway aMotorway = new Motorway();
+            Motorway bMotorway = new Motorway("Route 44", "Parkway", "W");
+            Motorway cMotorway = new Motorway("A14", "British Trunk Road", "ESE", "UK Highways Agency", 4);
             WriteLine("Hello World!");
         }
     }
