2c28dde0206c475248a0b14c7faeb74d46c21fe2
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-28 09:27:57 -0500

    Declare private data members

--- Motorway.cs
+++ Motorway.cs
@@ -6,5 +6,13 @@ namespace motorway_classes
 {
     class Motorway
     {
+        // private data members
+        private string highwayName,
+                       streetType,
+                       direction,
+                       surface,
+                       maintainedBy;
+        private int numberOfLanes;
+        private bool tollCharged;
     }
 }
