a2aa0ac696504806e9a3f9ea2c3e392bbda150c9
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-28 10:46:47 -0500

    Test instance methods

--- Program.cs
+++ Program.cs
@@ -11,7 +11,11 @@ namespace motorway_classes
             Motorway aMotorway = new Motorway();
             Motorway bMotorway = new Motorway("Route 44", "Parkway", "W");
             Motorway cMotorway = new Motorway("A14", "British Trunk Road", "ESE", "UK Highways Agency", 4);
-            WriteLine("Hello World!");
+            // test instance methods
+            WriteLine(aMotorway.FullName());
+            WriteLine(aMotorway.HasToll());
+            WriteLine(aMotorway.HasLanes());
+            WriteLine(aMotorway.ToString());
         }
     }
 }
