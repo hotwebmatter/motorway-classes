523b75122a2800ff73266f4b52acddb048b137de
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-28 09:29:03 -0500

    Implement public properties (setters and getters)

--- Motorway.cs
+++ Motorway.cs
@@ -14,5 +14,83 @@ namespace motorway_classes
                        maintainedBy;
         private int numberOfLanes;
         private bool tollCharged;
+        // public properties
+        public string HighwayName
+        {
+            set
+            {
+                highwayName = value;
+            }
+            get
+            {
+                return highwayName;
+            }
+        }
+        public string StreetType
+        {
+            set
+            {
+                streetType = value;
+            }
+            get
+            {
+                return streetType;
+            }
+        }
+        public string Direction
+        {
+            set
+            {
+                direction = value;
+            }
+            get
+            {
+                return direction;
+            }
+        }
+        public string Surface
+        {
+            set
+            {
+                surface = value;
+            }
+            get
+            {
+                return surface;
+            }
+        }
+        public string MaintainedBy
+        {
+            set
+            {
+                maintainedBy = value;
+            }
+            get
+            {
+                return maintainedBy;
+            }
+        }
+        public int NumberOfLanes
+        {
+            set
+            {
+                numberOfLanes = value;
+            }
+            get
+            {
+                return numberOfLanes;
+            }
+        }
+        public bool TollCharged
+        {
+            set
+            {
+                tollCharged = value;
+            }
+            get
+            {
+                return tollCharged;
+            }
+        }
     }
 }
